1037b6b542d5a37d374819e7a8f5bd4564a27b64
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-04-16 23:09:35 +0200

    added signed in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the AlphaBlog #{ @user.username }, you have signed up successfully"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
