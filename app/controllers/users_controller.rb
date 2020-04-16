f54337a863c4cd1546921d1098f4742e7755e557
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-04-16 03:02:25 +0200

    added user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
 
@@ -14,11 +15,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update 
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to user_path
@@ -42,4 +41,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+end
+
+def set_user
+  @user = User.find(params[:id])
 end
